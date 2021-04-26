42d7068a3bd0f181ccb01633def2c526f32b029d
Zach T <xslx_tnt@yahoo.com>
2021-04-25 23:12:59 -0400

    Cleaning up names

--- FortniteSettingsParser/Properties/FFortActorRecord.cs
+++ FortniteSettingsParser/Properties/FFortActorRecord.cs
@@ -8,36 +8,37 @@ namespace FortniteSettingsParser.Properties
 {
     public class FFortActorRecord : UStruct
     {
-        public byte Unknown { get; private set; }
+        public EFortBuildingPersistentState ActorState { get; private set; }
         public string ActorPath { get; private set; }
-        public FQuat Unknown4 { get; private set; } = new FQuat();
+        public FQuat Rotation { get; private set; } = new FQuat();
         public FVector3D Location { get; private set; } = new FVector3D();
         public FVector3D Scale { get; private set; } = new FVector3D();
-        public int Unknown5 { get; private set; }
+        public bool SpawnedActor { get; private set; }
 
         public int PropertyByteSize { get; private set; }
-        public Dictionary<string, UProperty> Properties { get; private set; }
+        public Dictionary<string, UProperty> ActorData { get; private set; }
 
         private byte[] UnknownExtraBytes;
 
         protected internal override void DeserializeProperty(UnrealBinaryReader reader)
         {
             Value = reader.ReadGuid();
-            Unknown = reader.ReadByte();
+            ActorState = (EFortBuildingPersistentState)reader.ReadByte();
             ActorPath = reader.ReadFString();
 
-            Unknown4.DeserializeProperty(reader);
+            Rotation.DeserializeProperty(reader);
             Location.DeserializeProperty(reader);
             Scale.DeserializeProperty(reader);
 
-            Unknown5 = reader.ReadInt32();
+            SpawnedActor = reader.ReadInt32() == 1;
+
             PropertyByteSize = reader.ReadInt32();
 
             long currentPosition = reader.BaseStream.Position;
 
             if(PropertyByteSize > 0)
             {
-                Properties = reader.ReadProperties();
+                ActorData = reader.ReadProperties();
                 reader.ReadInt32(); //?
             }
 
@@ -49,4 +50,14 @@ namespace FortniteSettingsParser.Properties
             }
         }
     }
+    public enum EFortBuildingPersistentState
+    {
+        Default,
+        New,
+        Constructed,
+        Destroyed,
+        Searched,
+        None,
+        EFortBuildingPersistentState_MAX,
+    };
 }
