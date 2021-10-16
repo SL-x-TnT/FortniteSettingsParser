6a83b08b76b8165d83b7139884d55ca7755d3f7b
Thomas <40025225+thoo0224@users.noreply.github.com>
2021-10-16 19:02:18 +0200

    fixed syntax error

--- src/FortniteSettingsParser/Property/FBoolProperty.cs
+++ src/FortniteSettingsParser/Property/FBoolProperty.cs
@@ -3,14 +3,11 @@
     public class FBoolProperty : UProperty
     {
         protected internal override void PreDeserializeProperty(UnrealBinaryReader reader)
-            => Value = reader.Read<bool>());
+            => Value = reader.Read<bool>();
 
         protected internal override void DeserializeProperty(UnrealBinaryReader reader)
         {
-            if(Value == null)
-            {
-                Value = reader.Read<bool>();
-            }
+            Value ??= reader.Read<bool>();
         }
     }
 }
